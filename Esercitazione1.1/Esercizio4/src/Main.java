e4d8ddfc105e4abb25214ef749e65f72cd4491d4
Angelica Cortez <a.cortez@campus.unimib.it>
2021-02-08 22:57:43 +0100

    Esercizio 4: matrice trasposta

---
+++ Esercitazione1.1/Esercizio4/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Costruiamo la matrice");
+        System.out.println("Numero di righe: ");
+        int righe =input.nextInt();
+        System.out.println("Numero di colonne: ");
+        int colonne =input.nextInt();
+        int[][] A = new int[righe][colonne];
+        int[][] AT= new int[colonne][righe];
+        System.out.println("Riempiamo la matrice ");
+        for(int i=0; i<righe; i++){
+            for (int j=0; j<colonne; j++){
+                System.out.println("Valore al ["+ i+"] ["+j+"]");
+                A[i][j]=input.nextInt();
+            }
+        }
+        for (int i = 0; i<righe; i++){
+            for(int j=0; j<colonne; j++){
+                AT[j][i]=A[i][j];
+            }
+        }
+        for(int i=0; i<righe; i++){
+            for(int j=0; j<colonne; j++){
+                System.out.print(A[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i=0; i<colonne; i++){
+            for(int j=0; j<righe; j++){
+                System.out.print(AT[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
